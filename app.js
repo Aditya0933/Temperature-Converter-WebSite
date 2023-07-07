3843be9240554d2d2a50ec52d68c8e00204aea71
Aditya Singh Parihar <201037@kit.ac.in>
2023-07-07 23:53:18 +0530

    Remove Extra Content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log('rerr')
-let celsiusInput = document.querySelector('.celsius > .icon > input')
-let fahrenheitInput = document.querySelector('.fahrenheit > .icon > input')
-let kelvinInput = document.querySelector('.kelvin > .icon > input')
+console.log('start')
+let celsiusInput = document.querySelector('.celsius > input')
+let fahrenheitInput = document.querySelector('.fahrenheit > input')
+let kelvinInput = document.querySelector('.kelvin > input')
 
 let btn = document.querySelector('.button button')
 
@@ -48,97 +48,4 @@ btn.addEventListener('click', ()=>{
     fahrenheitInput.value = ""
     kelvinInput.value = ""
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("Hello")
-// let CelciusInput = document.querySelector('#container-1 > input')
-// let FahrenheitInput = document.querySelector('#container-2 > input')
-// let KelvinInput = document.querySelector('#container-3 > input')
-
-// let btn = document.querySelector('.main-converter-btn > button')
-
-// function roundnumber(number) {
-//     return Math.round(number*100)/100
-// }
-
-// /*Celcius to Fahrenheight and Kelvin */
-// CelciusInput.addEventListener('click',function(){
-//     let cTemp = parseFloat(CelciusInput.value)
-//     let fTemp = (cTemp*(9/5)) + 32
-//     let kTemp = cTemp + 273.15
-
-//     FahrenheitInput.value = roundnumber(fTemp)
-//     KelvinInput.value = roundnumber(kTemp)
-// })
-
-// /*Fahrenheight to Celcius and Kelvin */
-// FahrenheitInput.addEventListener('input',function(){
-//     let fTemp = parseFloat(FahrenheitInput.value)
-//     let cTemp = (fTemp - 32) * (5/9)
-//     let kTemp = (fTemp - 32) * (5/9) + 273.15
-
-//     CelciusInput.value = roundnumber(cTemp)
-//     KelvinInput.value = roundnumber(kTemp)
-// })
-
-// /*Kelvin to Fahrenheight and Celcius */
-// KelvinInput.addEventListener('input',function(){
-//     let kTemp = parseFloat(KelvinInput.value)
-//     let cTemp = kTemp - 273.15
-//     let fTemp = (kTemp - 273.15) * (9/5) +32
-
-//     FahrenheitInput.value = roundnumber(fTemp)
-//     CelciusInput.value = roundnumber(kTemp)
-// })
-
-// /* ALL Clear Button */
-// btn.addEventListener('click', () => {
-//     console.log("oke")
-//     CelciusInput.value = " "
-//     FahrenheitInput.value = " "
-//     KelvinInput.value = " "
-//     console.log("done")
-// })
-
+console.log('End')
